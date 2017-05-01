14857284363d1739bf4b9a79e504429d01e2e31b
urotropine <urotropine99@gmail.com>
2017-04-30 20:17:33 -0400

    Update douban_scraper.py

--- douban_scraper.py
+++ douban_scraper.py
@@ -20,8 +20,8 @@ for page in [0*num,num,2*num,3*num,4*num,5*num,6*num,7*num]:  #pages to iterate
     comments = []
     for tag in tags:
         line = str(tag)
-	title = re.findall('\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s([^><]+)\n\n', line)
-	comment = re.findall('class="inq">(.+)<', line)
+	title = re.findall('\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s([^><]+)\n\n', line)     #regular expression: title
+	comment = re.findall('class="inq">(.+)<', line)                                #regular expression: comment
 	
 	for element in title:
 	    if element != []:
@@ -31,13 +31,6 @@ for page in [0*num,num,2*num,3*num,4*num,5*num,6*num,7*num]:  #pages to iterate
 	for element in comment:
 	    if element != []:
 		comments.append(element)
-
-#    print len(alist)
-#    for element in alist:
-#        print str(element).decode('string_escape')
-#    print len(comments)
-#    for element in comments:
-#	print str(element).decode('string_escape')
 
     for i in range(25):
 	print str(alist[i]).decode('string_escape'),'----', str(comments[i]).decode('string_escape')
